04a9bacf72db6887be120c25018a2c134d0ae9d6
FredQiu00 <tgao.fred@gmail.com>
2023-06-07 16:38:50 -0500

    Complete PUT method, the stock can be changed due to checkout items

--- webpage/src/Components/CartDisplay.js
+++ webpage/src/Components/CartDisplay.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { CartContext } from './CartContext';
 import './CartDisplay.css';
 
-const CartDisplay = () => {
+const CartDisplay = ({ products }) => {
   const { cart, isCartVisible, toggleCartVisibility, clearCart, removeFromCart } = useContext(CartContext);
 
   if (!isCartVisible) {
@@ -20,10 +20,33 @@ const CartDisplay = () => {
 
   const totalPrice = cart.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2);
 
-  const handleCheckout = () => {
+  const handleCheckout = async () => {
     if (cart.length > 0) {
-      alert(`Checkout completed. Total price: $${totalPrice}`);
-      clearCart();
+      try {
+        const updatePromises = [];
+
+        for (const item of cart) {
+          const { id, prod_name, description, price, quantity } = item;
+          const product = products.find((product) => product._id === id);
+          if (product) {
+            const updatedQuantity = product.quantity - quantity;
+            const updatePromise = fetch(`http://localhost:8000/api/products/${id}`, {
+              method: 'PUT',
+              body: JSON.stringify({ prod_name: prod_name, description: description, price: price, quantity: updatedQuantity }),
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+            updatePromises.push(updatePromise);
+          }
+        }
+        await Promise.all(updatePromises);
+        alert(`Checkout completed. Total price: $${totalPrice}`);
+        clearCart();
+      } catch (err) {
+        alert('An error occurred during checkout. Please try again.');
+      }
+
     } else {
       alert('Your cart is empty.');
     }
@@ -56,11 +79,7 @@ const CartDisplay = () => {
               >
                 {[...Array(item.quantity + 1).keys()].map((number) => (
                 <option key={number} value={number}>
-                  {number === 0 ? (
-                    <span>{number} (Remove item)</span>
-                  ) : (
-                    <span>{number}</span>
-                  )}
+                  {number === 0 ? `Remove item` : number}
                 </option>
                 ))}
               </select>
