c70e54d3c6c84edc1236b4eab837d7f739a134e8
FredQiu00 <tgao.fred@gmail.com>
2023-07-07 15:54:19 -0500

    3D secure adaptive

--- webpage/src/Pay/CheckoutBox.js
+++ webpage/src/Pay/CheckoutBox.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Modal } from 'react-bootstrap';
 import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
+import { getCountry, getCountryByAbbreviation } from 'country-currency-map';
+import { getNames, getCode } from 'country-list';
 import './CheckoutBox.css';
 
 const CheckoutBox = ({ user, totalPrice, handleCheckout }) => {
@@ -8,9 +10,13 @@ const CheckoutBox = ({ user, totalPrice, handleCheckout }) => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [zipCode, setZipCode] = useState('');
+  const [email, setEmail] = useState(user ? user.email : '');
+  const [phone, setPhone] = useState(user ? user.phone : '');
+  const [country, setCountry] = useState('');
 
   const stripe = useStripe();
   const elements = useElements();
+  const countries = getNames();
 
   if (!stripe || !elements) {
     return;
@@ -28,29 +34,21 @@ const CheckoutBox = ({ user, totalPrice, handleCheckout }) => {
 
     if (!error) {
       console.log(paymentMethod);
-      const response = await fetch('http://localhost:8000/api/payment', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
+      const convertedCountry = getCountryByAbbreviation(country);
+      const currency = getCountry(convertedCountry).currency.toLowerCase();
+      const paymentData = {
           amount: Math.round(totalPrice), // Amount to charge in cents
           paymentMethod: paymentMethod,
           name: `${firstName} ${lastName}`,
+          currency: currency,
           description : user ? `${user.username} purchased` : 'Guest purchase',
-          email: user ? user.email : 'guest@email.com',
-          phone: user ? user.phone : '000000000',
+          email: email,
+          phone: phone,
           zipCode: zipCode,
-        }),
-      });
-      const data = await response.json();
-      if (response.ok) {
-        alert(`${data.orderId}: ${data.message}`);
+        }
+
+        handleCheckout(paymentData, stripe); // Call your handleCheckout function here
         setShow(false);
-        handleCheckout(); // Call your handleCheckout function here
-      } else {
-        console.log('Payment failed');
-      }
     }
   };
 
@@ -67,26 +65,28 @@ const CheckoutBox = ({ user, totalPrice, handleCheckout }) => {
         <Modal.Body>
           <div className="payment-container">
             <Form onSubmit={handleSubmit} className="payment-form">
-              <Form.Group controlId="formFirstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                  placeholder="Enter your first name"
-                  required
-                />
-              </Form.Group>
-              <Form.Group controlId="formLastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                  placeholder="Enter your last name"
-                  required
-                />
-              </Form.Group>
+            <div className="name-field">
+                <Form.Group controlId="formFirstName" className="half-width-input">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    placeholder="Enter your first name"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId="formLastName" className="half-width-input">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder="Enter your last name"
+                    required
+                  />
+                </Form.Group>
+              </div>
               <Form.Group controlId="formCardElement">
                 <Form.Label>Card Information</Form.Label>
                 <CardElement
@@ -108,27 +108,56 @@ const CheckoutBox = ({ user, totalPrice, handleCheckout }) => {
                   className="StripeElement"
                 />
               </Form.Group>
-              <Form.Group controlId="formCountry">
-                <Form.Label>Country</Form.Label>
-                <Form.Control as="select" required>
-                  <option value="">Select a country</option>
-                  <option value="US">United States</option>
-                  <option value="CA">Canada</option>
-                  {/* Add more country options */}
-                </Form.Control>
-              </Form.Group>
-              <Form.Group controlId="formZipCode">
-                <Form.Label>Zip Code</Form.Label>
+              <div className="location-field">
+                <Form.Group controlId="formCountry" className="half-width-input">
+                  <Form.Label>Country</Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={country}
+                    onChange={(e) => setCountry(e.target.value)}
+                    required>
+                    <option value="">Select a country</option>
+                    {countries.map((c, index) => (
+                      <option key={index} value={getCode(c)}>
+                        {c}
+                      </option>
+                    ))}
+                  </Form.Control>
+                </Form.Group>
+                <Form.Group controlId="formZipCode" className="half-width-input">
+                  <Form.Label>Zip Code</Form.Label>
+                  <Form.Control
+                    type="number"
+                    value={zipCode}
+                    onChange={(e) => setZipCode(e.target.value)}
+                    placeholder="Enter your zip code" required
+                  />
+                </Form.Group>
+              </div>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email address</Form.Label>
                 <Form.Control
-                  type="number"
-                  value={zipCode}
-                  onChange={(e) => setZipCode(e.target.value)}
-                  max={99999}
-                  placeholder="Enter your zip code" required
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder={user ? '' : "Enter your email"}
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formPhone">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control
+                  type="tel"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+                  placeholder={user ? '' : "Enter your phone number"}
+                  required
                 />
               </Form.Group>
               <Button variant="primary" type="submit" className="payment-form-button">
-                Pay
+                Proceed with payment
               </Button>
             </Form>
           </div>
