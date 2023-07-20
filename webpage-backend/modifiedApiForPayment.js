e9e5da29fcc1a2921b486449fd0dab6d5eb44c81
FredQiu00 <tgao.fred@gmail.com>
2023-07-20 09:46:26 -0500

    added customer func

--- webpage-backend/modifiedApiForPayment.js
+++ webpage-backend/modifiedApiForPayment.js
@@ -3,12 +3,14 @@ const getItem = (item) => {
 }
 
 app.post('/api/payment-intent', async(req, res) => {
-  const { currency, amount, customer } = req.body;
+  const { currency, amount, currCustomer } = req.body;
   try {
+    const customer = await stripe.customers.create(currCustomer);
+    console.log(customer.id);
     const paymentIntent = await stripe.paymentIntents.create({
       currency: getItem(currency),
       amount: getItem(amount),
-      customer: getItem(customer),
+      customer: getItem(customer.id),
       automatic_payment_methods: {
         enabled: true,
       },
